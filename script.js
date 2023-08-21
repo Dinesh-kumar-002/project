adea2820c9683fefc67b8580d09f61a34abad4f3
Dinesh-Kumar-002 <dkdon812002@gmail.com>
2023-08-21 07:08:48 +0530

    200823

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 AOS.init();
+const lenis = new Lenis()
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf);
 var typing=new Typed(".write", {
     strings: ["", " \"RS Naturals Spices: Where Purity, Aroma, and Taste Converge for Unforgettable Meals\"", "\"Flavoring Life's Adventures: RS Naturals - Your Partner in Pure and Authentic Spices\""," \"RS Naturals Spices: Where Purity, Aroma, and Taste Converge for Unforgettable Meals\""," \"Savor the Genuine Taste of Nature: RS Naturals - Crafting Remarkable Spice Blends\""],
     typeSpeed: 100,
     backSpeed: 10,
     loop: true,
 });
-var win=document.querySelector('.navbar-container');
-window.onscroll = function() {scrollFunction()};
+// var win=document.querySelector('.navbar-container');
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if ( document.documentElement.scrollTop > 150) {
-    win.style.backgroundColor="black"
-} else {
-    win.style.backgroundColor="black"
-}
-}
+// function scrollFunction() {
+//   if ( document.documentElement.scrollTop > 150) {
+//     win.style.backgroundColor="black"
+// } else {
+//     win.style.backgroundColor="black"
+// }
+// }
 //menu bar 
 
 var offcanvas_body=document.querySelector('.offcanvas-body');
