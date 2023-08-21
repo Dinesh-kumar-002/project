a98173f21ad2fe16c20b645841725065ed1a8908
Dinesh-Kumar-002 <dkdon812002@gmail.com>
2023-08-21 10:48:06 +0530

    210820231048

--- script.js
+++ script.js
@@ -3,18 +3,33 @@ var typing=new Typed(".write", {
     typeSpeed: 100,
     backSpeed: 10,
     loop: true,
+    showCursor: false  
 });
-// var win=document.querySelector('.navbar-container');
-// window.onscroll = function() {scrollFunction()};
 
-// function scrollFunction() {
-//   if ( document.documentElement.scrollTop > 150) {
-//     win.style.backgroundColor="black"
-// } else {
-//     win.style.backgroundColor="black"
-// }
-// }
-//menu bar 
+
+gsap.from('.branding',{duration:2,y:'-150px',ease:'back'})
+gsap.from('.navbar-button',{duration:2,y:'-150px',ease:'back'})
+
+// var tl3 = gsap.timeline({
+//     scrollTrigger: {
+//         trigger: ".our-products",
+//         start: "top center",
+//         end: "bottom bottom",
+//         toggleActions: "play none none reset",
+//         // markers: true,
+//     },
+// });
+// tl3.fromTo('.our-products',{y:'-50px',opacity:'0'},{duration:2,y:'0px',opacity:'1',ease:'elastic',start:'top center',end:'bottom center',})
+var win=document.querySelector('.main');
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if ( document.documentElement.scrollTop > 150) {
+    win.style.backgroundColor="black"
+} else {
+    win.style.backgroundColor="transparent"
+}
+}
 
 var offcanvas_body=document.querySelector('.offcanvas-body');
 var close=document.querySelector('.close');
@@ -32,3 +47,10 @@ close.addEventListener('click', ()=>{
     })
 })
 
+
+// preloader
+
+window.addEventListener('load',()=>{
+    let preloader=document.querySelector('.preloader');
+    preloader.style.zIndex='0';
+})
